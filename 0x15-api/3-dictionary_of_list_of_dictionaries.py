6863f83959b12a2eb04ebb5121eef464c197b4db
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 20:22:58 -0500

    [FIX]

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Python3 Example of dictionary Python3 Example of dictionary """
+"""Python3 Example of dictionary Python3 Example of dictionary"""
 import json
 import request
 
