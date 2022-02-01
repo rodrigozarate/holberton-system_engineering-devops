779de145924e88d369f06bf8faaf2033bdc1c51b
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 22:14:50 -0500

    [TEST]

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -5,12 +5,4 @@ import json
 import requests
 
 if __name__ == "__main__":
-    filename = "todo_all_employees.json"
-    req = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-    req_id = requests.get('https://jsonplaceholder.typicode.com/users/').json()
-    with open(filename, "w") as dictfile:
-        dataset = {j.get("id"): [{'username': j.get('username'),
-                   'task': i.get('title'), 'completed': i.get('completed')}
-             for i in req
-             if j.get("id") == i.get('userId')] for j in req_id}
-json.dump(dataset, dictfile)
+    print('Vaca')
