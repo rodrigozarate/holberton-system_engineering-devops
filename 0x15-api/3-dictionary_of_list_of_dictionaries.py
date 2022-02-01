7e590a0530bebfcbc15e90858e290cb1162d611f
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 23:36:35 -0500

    [FIX]

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     req = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     req_id = requests.get('https://jsonplaceholder.typicode.com/users/').json()
     with open(filename, "w") as dictfile:
-        dataset = {j.get("id"): [{'task': i.get('title'),
-                   'completed': i.get('completed'),
-                                 'username': j.get('username')} for i in req
-                                 if j.get("id") == i.get('userId')]
+        dataset = {j.get("id"): [{'username': j.get('username'),
+                                 'task': i.get('title'),
+                                  'completed': i.get('completed')}
+                   for i in req if j.get("id") == i.get('userId')]
                    for j in req_id}
         json.dump(dataset, dictfile)
