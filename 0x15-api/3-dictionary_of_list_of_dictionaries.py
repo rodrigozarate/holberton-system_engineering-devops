0f64295dcead5ef6e0c1cc0b2f1aeb1a09871888
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 19:51:54 -0500

    [CREATE]

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Python3 Example of ditionary """
+import json
+import request
+
+
+if __name__ == '__main__':
+    filename = "todo_all_employees.json"
+    req = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    req_id = requests.get('https://jsonplaceholder.typicode.com/users/').json()
+    with open(filename, "w") as dictfile:
+        dataset = {j.get("id"): [{'username': j.get('username'), 'task': i.get('title'),
+             'completed': i.get('completed')} for i in req
+                           if j.get("id") == i.get('userId')]
+             for j in req_id}
+        json.dump(dataset, dictfile)
