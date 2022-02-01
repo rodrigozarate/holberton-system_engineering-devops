8982739da4a272b79de0c2bceb35bffcbe8993c4
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 22:13:33 -0500

    [TEST]

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -4,7 +4,7 @@
 import json
 import requests
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     filename = "todo_all_employees.json"
     req = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     req_id = requests.get('https://jsonplaceholder.typicode.com/users/').json()
