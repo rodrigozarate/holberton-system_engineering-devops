bb13b0377cadad35008c927b67d4c1f2a76651ea
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 21:25:27 -0500

    [TEST]

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
-"""Some kind of words regarding https://jsonplaceholder.typicode.com"""
+"""
+Some kind of words regarding https://jsonplaceholder.typicode.com
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ac convallis ex,
+sed euismod nisl. Sed laoreet volutpat imperdiet. Proin at lacinia diam.
+Nunc suscipit vulputate tristique. Nam ac mauris dolor. Donec tincidunt vehicula
+pellentesque. Praesent lobortis urna vitae sem condimentum iaculis.
+Nullam posuere eros nec justo accumsan dictum. Cras ex quam, congue a aliquet et,
+porta a mi. Nunc varius feugiat dui, sit amet sodales lorem laoreet quis.
+Donec ornare nunc cursus posuere lacinia. Etiam eu mauris mi. Vivamus sit amet
+volutpat libero, vel pretium nulla. Maecenas tempus magna vitae finibus congue.
+Sed vehicula fringilla scelerisque. Nam blandit ultricies vestibulum.
+"""
 import json
 import request
 
