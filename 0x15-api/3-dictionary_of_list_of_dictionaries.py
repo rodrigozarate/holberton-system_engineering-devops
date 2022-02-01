8c837333211ae883730b270fee2d0774c66063d5
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 21:14:09 -0500

    [TEST]

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,16 @@
+'''Some kind of words regarding https://jsonplaceholder.typicode.com'''
+#!/usr/bin/python3
+import json
+import request
+
+
+if __name__ == '__main__':
+    filename = "todo_all_employees.json"
+    req = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    req_id = requests.get('https://jsonplaceholder.typicode.com/users/').json()
+    with open(filename, "w") as dictfile:
+        dataset = {j.get("id"): [{'username': j.get('username'),
+                   'task': i.get('title'), 'completed': i.get('completed')}
+             for i in req
+             if j.get("id") == i.get('userId')] for j in req_id}
+        json.dump(dataset, dictfile)
