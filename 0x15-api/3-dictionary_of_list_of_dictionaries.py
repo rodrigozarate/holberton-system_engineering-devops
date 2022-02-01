d0c6d25dfd797f8ed46c6736d8bacbe67de5f8eb
Rodrigo Zárate Algecira <rodrigo@zarate.com.co>
2022-01-31 22:00:17 -0500

    [TEST]

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,20 +8,4 @@ import json
 import requests as rq
 
 if __name__ == "__main__":
-    ids = [i for i in range(1, 11)]
-    session = rq.Session()
-    info = {}
-    for id in ids:
-        URL_users = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-        URL_ = "https://jsonplaceholder.typicode.com/users/{}/todos".format(id)
-        response = session.get(URL_users).json()
-        username = response['username']
-        task = session.get(URL_).json()
-        tasks = []
-        info[id] = tasks
-        for content in task:
-            tasks.append({"username": username,
-                          "task": content['title'],
-                          "completed": content['completed']})
-    with open("todo_all_employees.json", "w") as file:
-        json.dump(info, file)
+    print('Hello')
